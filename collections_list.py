eb402ed3f562d9581a3d8d6b6fd5f73f092571c9
Connor Murphy <connor@murphysites.com>
2023-11-21 19:02:40 -0500

    clean up for readability

--- collections_list.py
+++ collections_list.py
@@ -5,22 +5,27 @@ from columnar import columnar
 def get_bdr_collections():
     '''returns a dict like "'collection name':[number of items]"'''
 
-    collection_field = 'ir_collection_name' #field to facet on
+    collection_field = 'ir_collection_name' # field to facet on
     bdr_api = 'https://repository.library.brown.edu/api/search/'
     solr_query = f'?q=*&facet=on&facet.field={collection_field}&rows=0'
     query_result = urllib.request.urlopen(bdr_api + solr_query).read()
-    facet_counts = json.loads(query_result)['facet_counts']['facet_fields'][collection_field] #drill down to list
+    qjson = json.loads(query_result)
+    # drill down to list
+    facet_counts = qjson['facet_counts']['facet_fields'][collection_field]
     
-    #facet_counts is a list like ['collection name',number of items,'collection name',number of items, ...]
-    #so, make dict like {facet_counts[0]:facet_counts[1],facet_counts[2]:facet_counts[3], ...]
+    # facet_counts is a list like: 
+    #   ['collection name', # items,'collection name', # items, ...]
+    # so, make dict like:
+    #   {facet_counts[0]:facet_counts[1],facet_counts[2]:facet_counts[3], ...]
     facet_iter = iter(list(facet_counts))
     result = {i:next(facet_iter) for i in facet_iter}
 
     return result 
 
 if __name__ == '__main__':
-    #use columnar to make printout show columns
+    # use columnar to make printout show columns
     bdr_collections = get_bdr_collections()
-    collection_table = [[key,value] for key,value in bdr_collections.items()]
-    print(columnar(collection_table,headers=['collection', 'no. items'],no_borders=True))
+    colls_table = [[key,value] for key,value in bdr_collections.items()]
+    table_headers = ['collection','no. items']
+    print(columnar(colls_table,headers=table_headers,no_borders=True))
     print(f'Total number of collections: {len(bdr_collections)}')
