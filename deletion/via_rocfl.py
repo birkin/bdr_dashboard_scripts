82f8174d2b7a50b71b271aa99ed906136f30d4ee
Birkin James Diana <birkin.diana@gmail.com>
2024-01-23 08:28:44 -0500

    stubs deletion function.

--- deletion/via_rocfl.py
+++ deletion/via_rocfl.py
@@ -1,3 +1,15 @@
+import logging
+
+## 3rd party
 from dotenv import load_dotenv, find_dotenv
 
+## load envars
 load_dotenv( find_dotenv(raise_error_if_not_found=True) )
+STORAGE_ROOT_PATH = os.environ['DEL__STORAGE_ROOT_PATH']
+
+
+def delete_item( pid ):
+    print( f"Deleting {item_id}" )
+## cd to repo root
+
+
