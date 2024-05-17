2ca0b9595d44e972ae091bfe49104736582fdbde
Birkin James Diana <birkin.diana@gmail.com>
2024-05-17 09:21:22 -0400

    tweaks docstring path.

--- update_hhoag_mods/02_gather_mods_paths.py
+++ update_hhoag_mods/02_gather_mods_paths.py
@@ -3,5 +3,5 @@
     (eg `HH123456` for orgs, or `HH123456_0001` for items). 
 Purpose: this json file will be enhanced with bdr-pids in a later step.
 Usage: python ./update_hhoag_mods/02_gather_mods_paths.py
-Output: ./data_files/02b_hhoag_mods_paths.json
+Output: ./update_hhoag_mods/data_files/02b_hhoag_mods_paths.json
 """
